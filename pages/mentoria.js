83c00e685d55024a090303dd762cfb6e0d47f8de
GraceTorresLeite <grace.torresleite@gmail.com>
2021-02-22 09:52:40 -0300

    update pages

---
+++ pages/mentoria.js
@@ -0,0 +1,24 @@
+const Mentoria = () => {
+    const variavel = `ola`
+    return (
+        <>
+            <h1>Olá Mentoria!</h1>
+            <p>Tudo bom!</p>
+            {variavel}
+            <style jsx>{`
+                h1{
+                    color: red;
+            }
+            `}</style>
+        </>
+    )
+}
+
+export default Mentoria;
+
+/*const Mentoria = () => {
+    return (
+        <h1>Olá Mentoria!</h1>
+            )
+}
+        export default Mentoria;*/
